9e064941c2cf678e8c0f4ec7c2103331c7f75065
brook420 <adityaambure19@gmail.com>
2026-03-01 19:49:11 +0530

    chore: update Gemini key to latest provided one

--- test_gemini.cjs
+++ test_gemini.cjs
@@ -1,6 +1,6 @@
 const https = require('https');
 
-const apiKey = 'AIzaSyDK7rJ9JUWBikJZ34eONkmG_-tVj5qWcUs';
+const apiKey = 'AIzaSyBIpWbpmG4gZmnyo9XNm2JHezn9kstYKTE';
 const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`;
 
 const data = JSON.stringify({
